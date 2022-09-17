6d45e151bd1aba366f0ac5ff420480cd90226597
carly-west <carly.west@byui.edu>
2022-09-17 16:46:54 -0600

    styling

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,42 @@
-import { loadHeader } from "./utils.js";
+import { loadHeader } from "../js/utils.js";
 
 // Loads header
 loadHeader();
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp} from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB2NvowmMXThqn01P54Vm-0mSwuVIvbi1I",
+  authDomain: "duedate-402bc.firebaseapp.com",
+  projectId: "duedate-402bc",
+  storageBucket: "duedate-402bc.appspot.com",
+  messagingSenderId: "792845287352",
+  appId: "1:792845287352:web:c3110ba0023c7d32a5594d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+const user = auth.currentUser;
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    const uid = user.uid;
+    console.log("logged in- on auth state change")
+    document.getElementById("logout-btn").style.display = "block";
+    document.getElementById("login-btn").style.display = "none";
+    document.getElementById("register-btn").style.display = "none";
+    console.log(user);
+
+  } else {
+    // User is signed out
+    console.log("not logged in- on auth state change")
+    document.getElementById("logout-btn").style.display = "none";
+  }
+});
